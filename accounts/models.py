a42d56fa3bfc0322f0d4e75399fcde5d39d3e260
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-19 15:10:30 +0600

    created django bank project, created a app name accounts and created registration model inside account

---
+++ accounts/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from .constants import ACCOUNT_TYPE, GENDER_TYPE
+
+
+class UserBankAccount(models.Model):
+    user = models.OneToOneField(User, related_name='account', on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPE)
+    account_no = models.IntegerField(unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender =  models.CharField(max_length=10, choices=GENDER_TYPE)
+    initial_deposit_date = models.DateField(auto_now_add=True)
+    balance = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    
+    
+class UserAddress(models.Model):
+    user = models.OneToOneField(User, related_name='address', on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.IntegerField()
+    country = models.CharField(max_length=100)
