bfc97e2c32b28c1ee7fb93476980ae9362161c13
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-20 20:01:40 +0600

    implemented base templates into new app 'core' and login, logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import FormView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .forms import UserRegistrationForm
 
@@ -17,5 +18,18 @@ class UserRegistrationView(FormView):
         user = form.save()
         login(self.request, user)
         return super().form_valid(form)
+    
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
+     
             
     
