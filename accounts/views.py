317628273affbe06233001a9c221ceddba830b85
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-20 20:23:48 +0600

    update user profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import login, logout
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
-
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm
+from django.views import View
 
 # Create your views here.
 
@@ -30,6 +30,21 @@ class UserLogoutView(LogoutView):
         if self.request.user.is_authenticated:
             logout(self.request)
         return reverse_lazy('home')
+    
+    
+class UserBankAccountUpdateView(View):
+    template_name = 'accounts/profile.html'
+
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the user's profile page
+        return render(request, self.template_name, {'form': form})
      
             
     
