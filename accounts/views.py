b426f2369f1c0840e0617f5ea6cc54750b8b7a53
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-20 14:50:18 +0600

    created user registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import login
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import FormView
+
+from .forms import UserRegistrationForm
 
 # Create your views here.
+
+class UserRegistrationView(FormView):
+    template_name = ''
+    form_class = UserRegistrationForm
+    success_url = ''
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(user)
+        return super().form_valid(form)
+            
+    
