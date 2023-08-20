3bc7b4b14c231591e1d4b4470465f953cb95a2d4
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-21 01:38:10 +0600

    created a transaction model

---
+++ transactions/constants.py
@@ -0,0 +1,6 @@
+TRANSACTION_TYPE = (
+    (1, 'Deposit'),
+    (2, 'Withdraw'),
+    (3, 'Loan'),
+    (4, 'Loan Paid'),
+)
---
+++ transactions/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from .constants import TRANSACTION_TYPE
+from accounts.models import UserBankAccount 
+# Create your models here.
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(UserBankAccount, related_name='transactions', on_delete=models.CASCADE) # One user can have multiple transactions (loan, deposit, withdraw)
+    
+    amount = models.DecimalField(decimal_places=2, max_digits=12)
+    balance_after_transaction = models.DecimalField(decimal_places=2, max_digits=12)
+    transaction_type = models.IntegerField(choices=TRANSACTION_TYPE, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    loan_approve = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['timestamp']
+        
+
